fb5a23b82ea6f1ea37a9169e8da1020ca6bda387
davide <dacapoccia@gmail.com>
2022-09-01 19:15:03 +0200

    mlstn2: flags added

--- src/App.vue
+++ src/App.vue
@@ -37,5 +37,6 @@ export default {
 </script>
 
 <style lang="scss">
+@import '~@fortawesome/fontawesome-free/css/all.css';
 
 </style>
--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -4,7 +4,8 @@
         <li v-for="(movie, index) in movieList" :key="index">
             {{movie.title}}
             {{movie.original_title}}
-            {{movie.original_language}}
+            <img v-if="langFlags.includes(movie.original_language)" :src="require('../assets/flags/' + movie.original_language + '.png')" alt="">
+            <span v-else>{{movie.original_language}}</span>
             {{movie.vote_average }}
         </li>    
     </ul>
@@ -16,6 +17,11 @@ export default {
     name: "MyMain",
     props: {
         movieList: Array
+    },
+    data(){
+        return {
+            langFlags: ['it', 'en', 'fr', 'de', 'nl', 'es', 'jp', 'ch', 'por']
+        }
     }
 }
 </script>
