b6bd55b03292ff7b5c577950a7fd5eb76ba9c7f5
davide <dacapoccia@gmail.com>
2022-09-01 21:25:47 +0200

    milestone 2 done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <MyHeader @search="searching"/>
-    <MyMain :movieList="movieList"/>
+    <MyMain :movieList="movieList" :seriesList="seriesList"/>
   </div>
 </template>
 
@@ -22,14 +22,32 @@ export default {
       apiUrl: 'https://api.themoviedb.org/3',
       apiKey: 'a3ed888e36325b73e39c2587ed60534a',
       language: 'it-IT',
-      movieList: []
+      movieList: [],
+      seriesList: []
     }
   },
   methods: {
     searching(research){
-      axios.get(this.apiUrl + '/search/movie?api_key=' + this.apiKey + '&language=' + this.language + '&query=' + research)
+      const parameters = {
+        params: {
+        api_key: this.apiKey,
+        language: this.language,
+        query: research
+        } 
+      };
+      this.getMovies(parameters);
+      this.getSeries(parameters);
+    },
+    getMovies(parameters) {
+      axios.get(this.apiUrl + '/search/movie', parameters)
       .then(res =>{
         this.movieList = res.data.results;
+      })
+    },
+     getSeries(parameters) {
+      axios.get(this.apiUrl + '/search/tv', parameters)
+      .then(res =>{
+        this.seriesList = res.data.results;
       })
     }
   }
--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,22 +1,32 @@
 <template>
-  <div>
-    <ul>
+  <main>
+    <ul class="myMovies">
         <li v-for="(movie, index) in movieList" :key="index">
             {{movie.title}}
             {{movie.original_title}}
-            <img v-if="langFlags.includes(movie.original_language)" :src="require('../assets/flags/' + movie.original_language + '.png')" alt="">
+            <img class="flag" v-if="langFlags.includes(movie.original_language)" :src="require('../assets/flags/' + movie.original_language + '.png')" alt="">
             <span v-else>{{movie.original_language}}</span>
             {{movie.vote_average }}
         </li>    
     </ul>
-  </div>
+    <ul class="mySeries">
+        <li v-for="(serie, index) in seriesList" :key="index">
+            {{serie.name}}
+            {{serie.original_name}}
+            <img class="flag" v-if="langFlags.includes(serie.original_language)" :src="require('../assets/flags/' + serie.original_language + '.png')" alt="">
+            <span v-else>{{serie.original_language}}</span>
+            {{serie.vote_average }}
+        </li>    
+    </ul>
+  </main>
 </template>
 
 <script>
 export default {
     name: "MyMain",
     props: {
-        movieList: Array
+        movieList: Array,
+        seriesList: Array
     },
     data(){
         return {
@@ -27,5 +37,5 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+    .flag{max-width: 20px}
 </style>
