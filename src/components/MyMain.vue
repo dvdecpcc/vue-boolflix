b22bfbc30470f80a5a37b9f7ce6dce7e8d9a344e
davide <dacapoccia@gmail.com>
2022-09-01 17:06:18 +0200

    milestone 1 done

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <ul>
-        <li v-for="(movie, index) in movieList" :key="index"></li>
+        <li v-for="(movie, index) in movieList" :key="index">
             {{movie.title}}
             {{movie.original_title}}
             {{movie.original_language}}
             {{movie.vote_average }}
+        </li>    
     </ul>
   </div>
 </template>
